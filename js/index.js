817fc184c4c29471b5a82b819f4e4ea377540d49
Wiktor Gołębiewski <64951471+WGolebiewski@users.noreply.github.com>
2022-03-05 00:20:12 +0100

    Added decryption system

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-function getVal() {
-    const val = document.querySelector('input').value;
-    return encrypt(val);
+function getVal(en) {
+    if (en == true) {
+        return document.getElementById('encrytper').value;
+    } else {
+        return document.getElementById('decrypter').value;
+    }
 }
 
 function encrypt(pass) {
@@ -16,10 +19,30 @@ function encrypt(pass) {
         encryptedPass += String.fromCharCode(charCode);
         encryptedPass += String.fromCharCode(charCode + 1);
     }
-    console.log(encryptedPass);
     return encryptedPass;
 }
 
-function changeText() {
-    document.getElementById("passOutput").innerHTML = getVal();
+function changeText(en) {
+    if (en == true) {
+        document.getElementById("passOutputEncrypted").innerHTML = encrypt(getVal(true));
+    } else {
+        document.getElementById("passOutputDecrypted").innerHTML = decrypt(getVal(false));
+    }
+
+}
+
+function decrypt(pass) {
+    let decryptedPass = "";
+    for (let i = 0; i < pass.length; i++) {
+        let charCode = pass.charCodeAt(i);
+        for (let j = 0; j < 4; j++) {
+            if (charCode < 33) {
+                charCode = 127;
+            }
+            charCode--;
+        }
+        decryptedPass += String.fromCharCode(charCode);
+        i++;
+    }
+    return decryptedPass;
 }
